997e3fadd50a0c4af8eea9b94749ffaf7a69c777
Lorin-Lange <lo.-lange@arcor.de>
2024-06-05 17:35:59 +0200

    added solution for 'Find Nearest square number'

--- NearestSquare/NearestSquare.hs
+++ NearestSquare/NearestSquare.hs
@@ -7,7 +7,15 @@
 module NearestSquare where 
 
 nearestSquare :: Int -> Int
-nearestSquare n = test [x*x | x <- [0..]]
+nearestSquare n
+    | (u^2 - n) < (n - l^2) = u^2 
+    | otherwise             = l^2
+  where r = sqrt $ fromIntegral n
+        l = floor r
+        u = ceiling r
+
+nearestSquare' :: Int -> Int
+nearestSquare' n = test [x*x | x <- [0..]]
   where test (x1:x2:xs) | x1 == n          = n
                         | x1 < n && x2 > n = if (n - x1) < (x2 - n) then x1 else x2
                         | otherwise        = test (x2:xs)
